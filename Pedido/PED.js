d0be041568f21b03ffa2fa0ea6aa8ac8113c9504
nicholasdoulgas <revynax@gmail.com>
2024-02-23 08:52:59 -0300

    23

---
+++ Pedido/PED.js
@@ -0,0 +1,30 @@
+function comecar()
+{
+    var nome = document.getElementById("exampleFormControlInput1").value;
+    console.log(nome);
+    show(nome);
+}
+
+function show(nome)
+{
+    document.getElementById("nomePrint").textContent = nome;
+    var div = document.getElementById("select-div");
+
+    if(nome && nome.trim() !== "")
+    {
+        div.style.display = "block";
+        document.getElementById("exampleFormControlInput1").style.background = "none";
+    }
+
+    else
+    {
+        document.getElementById("error").style.display = "flex";
+    }
+}
+
+function fechar()
+{
+    document.getElementById("error").style.display = "none";
+    document.getElementById("exampleFormControlInput1").style.background = "lightpink";
+}
+ 
