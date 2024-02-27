448755f26c07d8ff097ec5b436a47e86f4f86b4f
nicholasdouglas <revynax@gmail.com>
2024-02-27 10:28:40 -0300

    27

--- Pedido/PED.js
+++ Pedido/PED.js
@@ -28,3 +28,55 @@ function fechar()
     document.getElementById("exampleFormControlInput1").style.background = "lightpink";
 }
  
+function receberVal()
+{
+    var pratoEsc = document.getElementById("pratoSel").value;
+    var bebidaEsc = document.getElementById("bebidaSel").value;
+    var sobremesaEsc = document.getElementById("sobremesaSel").value;
+
+    var valTotal = 0;
+
+    switch (pratoEsc) 
+    {
+        case "Tartar de carne":
+            valTotal = valTotal + 40;
+            break;
+    
+        case "Pizza portuguesa":
+            valTotal = valTotal + 20;
+            break;
+        case "Strogonoff de carne":
+            valTotal = valTotal + 15;
+            break;
+    }
+
+    switch (bebidaEsc) 
+    {
+        case "Pepsi":
+            valTotal = valTotal + 6;
+            break;
+    
+        case "Monster Pink":
+            valTotal = valTotal + 10;
+            break;
+        case "Martini azul":
+            valTotal = valTotal + 20;
+            break;
+    }
+
+    switch (sobremesaEsc) 
+    {
+        case "Banoffe":
+            valTotal = valTotal + 15;
+            break;
+    
+        case "Torta holandesa":
+            valTotal = valTotal + 15;
+            break;
+        case "Tortolini":
+            valTotal = valTotal + 20;
+            break;
+    }
+
+    console.log(valTotal);
+}
